e62d99161b3a817da089966961cd40a648462717
Prasiddha Khadka <be2018se630@gces.edu.np>
2023-01-03 15:45:47 +0545

    route setup

---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,27 @@
+import 'dart:js';
+
+import 'package:b_library/utils/routes/route_name.dart';
+import 'package:b_library/view/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case RouteName.splashScreen:
+        return MaterialPageRoute(builder: (context) => const SplashScreen());
+
+      default:
+        return MaterialPageRoute(
+            builder: (context) => const Scaffold(
+                  body: Center(
+                    child: Text(
+                      'No Such Routes Defined',
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                ));
+    }
+  }
+}
